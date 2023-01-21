6172549fbe824b0b2809b0194a6e2917683acc55
defiant00 <defiant00@gmail.com>
2023-01-20 18:52:46 -0800

    switch to just calculating compression sizes

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const img = @import("zigimg");
 
 // zig build run -- c:\git\test_images\
+// https://qoiformat.org/
 
 pub fn main() !void {
     std.debug.print("Image Encoding Test 0.0.1\n", .{});
@@ -15,7 +16,7 @@ pub fn main() !void {
     _ = args.next(); // skip program name
 
     const png = ".png";
-    const ext = ".zimg";
+    // const ext = ".zimg";
     var buf: [1024]u8 = undefined;
 
     while (args.next()) |path| {
@@ -33,27 +34,27 @@ pub fn main() !void {
                 var image = try img.Image.fromFilePath(allocator, buf[0 .. path.len + entry.name.len]);
                 defer image.deinit();
 
-                // save image
-                const name_len = entry.name.len - png.len;
-                std.mem.copy(u8, buf[0..], entry.name[0..name_len]);
-                std.mem.copy(u8, buf[name_len..], ext);
-                const file = try dir.dir.createFile(buf[0 .. name_len + ext.len], .{});
-                defer file.close();
-                var buffer = std.io.bufferedWriter(file.writer());
-                const writer = buffer.writer();
+                // // save image
+                // const name_len = entry.name.len - png.len;
+                // std.mem.copy(u8, buf[0..], entry.name[0..name_len]);
+                // std.mem.copy(u8, buf[name_len..], ext);
+                // const file = try dir.dir.createFile(buf[0 .. name_len + ext.len], .{});
+                // defer file.close();
+                // var buffer = std.io.bufferedWriter(file.writer());
+                // const writer = buffer.writer();
 
-                // magic bytes
-                try writer.writeAll("zimg");
-                // width
-                try writer.writeInt(u32, @intCast(u32, image.width), .Little);
-                // height
-                try writer.writeInt(u32, @intCast(u32, image.height), .Little);
-                // channels
-                try writer.writeByte(3);
-                // colorspace
-                try writer.writeByte(0);
+                // // magic bytes
+                // try writer.writeAll("zimg");
+                // // width
+                // try writer.writeInt(u32, @intCast(u32, image.width), .Little);
+                // // height
+                // try writer.writeInt(u32, @intCast(u32, image.height), .Little);
+                // // channels
+                // try writer.writeByte(3);
+                // // colorspace
+                // try writer.writeByte(0);
 
-                try buffer.flush();
+                // try buffer.flush();
             }
         }
     }
